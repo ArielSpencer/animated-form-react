f38597adac0a695ba21c783d3762142886bfb727
Ariel Spencer <arielspencer.tech@gmail.com>
2024-10-11 23:42:06 -0300

    feat(header): add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
 import './App.scss'
+import { ArrowIcon } from './components/arrow-icon'
+import { Logo } from './components/logo'
 
 function App() {
   return (
     <main>
-      <h1>my app</h1>
+      <header className='header_container'>
+        <Logo />
+        <nav className='menu_options'>
+          <a href="#">What we offer</a>
+          <a href="#">How it works</a>
+          <a href="#">Portfolio</a>
+        </nav>
+        <button>
+          <div className='btn-face-white'>
+            Contact us
+            <ArrowIcon />
+          </div>
+
+          <div className='btn-face-purple'>
+            Contact us
+            <ArrowIcon />
+          </div>
+
+        </button>
+      </header>
     </main>
   )
 }
